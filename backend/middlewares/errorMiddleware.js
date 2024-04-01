656746338c8f93bbfbc270dcd292a2c7ec27f535
Farooque <farooque.reactjs@gmail.com>
2024-04-01 14:14:52 +0530

    done

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -3,9 +3,9 @@ const errorHandler = (err, req, res, next) => {
   res.status(statusCode);
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    stack: process.env.NODE_ENV === "Production" ? null : err.stack,
   });
   next();
 };
 
-module.exports = errorHandler;
+module.exports = { errorHandler };
