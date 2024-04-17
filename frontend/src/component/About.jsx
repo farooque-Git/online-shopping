4ffc285f4aa801a0967fe8afc458609abc221e3d
Farooque <farooque.reactjs@gmail.com>
2024-04-18 00:41:42 +0530

    redux reducer

--- frontend/src/component/About.jsx
+++ frontend/src/component/About.jsx
@@ -1,8 +1,16 @@
 const About = () => {
   return (
     <div>
-    
-      <div>{<h1>About </h1>}</div>
+      <div>
+        {
+          <h2 style={{ padding: "10px", margin: "20px" }}>
+            Online Shopping technology drives path-breaking, customer-focused
+            innovation that makes high quality products accessible to Indian
+            shoppers, besides making the online shopping experience convenient,
+            intuitive and seamless.
+          </h2>
+        }
+      </div>
     </div>
   );
 };
--- frontend/src/component/Contact.jsx
+++ frontend/src/component/Contact.jsx
@@ -1,7 +1,7 @@
 const Contact = () => {
   return (
     <div>
-      <div>{<h1>Contact</h1>}</div>
+      <div>{<h1>This is a Contact</h1>}</div>
     </div>
   );
 };
