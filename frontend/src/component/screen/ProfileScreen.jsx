25dfaf36d0dd395fdc73e41c128a791fb3708bb8
Farooque <farooque.reactjs@gmail.com>
2024-05-11 00:53:43 +0530

    profile done

--- frontend/src/component/screen/ProfileScreen.jsx
+++ frontend/src/component/screen/ProfileScreen.jsx
@@ -11,11 +11,13 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Snackbar,
+  Box,
 } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../shared/Message";
 import Loader from "../shared/Loader";
-import { getUserDetails } from "../../actions/userAction";
+import { getUserDetails, updateUserProfile } from "../../actions/userAction";
 import FormContainer from "../shared/FormContainer";
 
 const ProfileScreen = () => {
@@ -28,14 +30,14 @@ const ProfileScreen = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const userDetails = useSelector((state) => state.userDetails);
-  const { loading, error, user } = userDetails;
   //   const userDetails = useSelector((state) => state.userDetails);
-  //   const {
-  //     loading: userDetailsLoading,
-  //     error: userDetailsError,
-  //     user,
-  //   } = userDetails || {};
+  //   const { loading, error, user } = userDetails;
+  const userDetails = useSelector((state) => state.userDetails);
+  const {
+    loading: userDetailsLoading,
+    error: userDetailsError,
+    user,
+  } = userDetails || {};
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
@@ -43,17 +45,23 @@ const ProfileScreen = () => {
   const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
   const { success } = userUpdateProfile;
 
-  const orderListMy = useSelector((state) => state.orderListMy);
-  const { loading: loadingOrders, orders, error: errorOrders } = orderListMy;
+  const orderListMy = useSelector((state) => state.orderListMy) || {};
+  const {
+    loading: loadingOrders,
+    orders = [],
+    error: errorOrders,
+  } = orderListMy;
 
   useEffect(() => {
-    // Redirect to the specified route if user info is available
+    // Redirect to the login page if user info is not available
     if (!userInfo) {
       navigate("/login");
     } else {
-      if (!user.name) {
+      // Fetch user details if not available
+      if (!user) {
         dispatch(getUserDetails("profile"));
       } else {
+        // Set name and email if available
         setName(user.name);
         setEmail(user.email);
       }
@@ -66,107 +74,126 @@ const ProfileScreen = () => {
       setMessage("Password does not Match !!!");
     } else {
       // Dispatch the login action
-      //   dispatch(updateUserProfile(name, email, password));
+      dispatch(updateUserProfile({ id: user._id, name, email, password }));
     }
   };
 
   return (
     <>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={3}>
-          <Typography variant="h4">Update Information</Typography>
-          {/* Display messages if there are errors, success or loading */}
-          <form onSubmit={submitHandler}>
-            <TextField
-              label="Name"
-              fullWidth
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <TextField
-              label="Email Address"
-              fullWidth
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              label="Password"
-              fullWidth
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <TextField
-              label="Confirm Password"
-              fullWidth
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            <Button type="submit" variant="contained" color="primary">
-              Update
-            </Button>
-          </form>
-        </Grid>
-        <Grid item xs={12} md={9}>
-          <Typography variant="h4">My Orders</Typography>
-          {/* Display loader, error or orders table */}
-          {loadingOrders ? (
-            <Loader />
-          ) : errorOrders ? (
-            <Message variant="danger">{errorOrders}</Message>
-          ) : (
-            <TableContainer>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>ID</TableCell>
-                    <TableCell>DATE</TableCell>
-                    <TableCell>TOTAL</TableCell>
-                    <TableCell>PAID</TableCell>
-                    <TableCell>DELIVERED</TableCell>
-                    <TableCell></TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {orders.map((order) => (
-                    <TableRow key={order._id}>
-                      <TableCell>{order._id}</TableCell>
-                      <TableCell>{order.createdAt.substring(0, 10)}</TableCell>
-                      <TableCell>{order.totalPrice}</TableCell>
-                      <TableCell>
-                        {order.isPaid ? (
-                          order.paidAt.substring(0, 10)
-                        ) : (
-                          <i
-                            className="fas fa-times"
-                            style={{ color: "red" }}
-                          ></i>
-                        )}
-                      </TableCell>
-                      <TableCell>
-                        {order.isDelivered ? (
-                          order.deliveredAt.substring(0, 10)
-                        ) : (
-                          <i
-                            className="fas fa-times"
-                            style={{ color: "red" }}
-                          ></i>
-                        )}
-                      </TableCell>
-                      <TableCell>
-                        <Link to={`/order/${order._id}`}>
-                          <Button variant="outlined">Details</Button>
-                        </Link>
-                      </TableCell>
+      <Box p={3}>
+        {" "}
+        {/* Add padding */}
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={3}>
+            <Typography variant="h4" mb={2}>
+              Update Profile
+            </Typography>{" "}
+            {/* Add margin bottom */}
+            <Snackbar open={success} autoHideDuration={6000}>
+              <Message variant="success">Profile Updated</Message>
+            </Snackbar>
+            <form onSubmit={submitHandler}>
+              <TextField
+                label="Name"
+                fullWidth
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                mb={3} // Add margin bottom
+              />
+              <TextField
+                label="Email Address"
+                fullWidth
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                mb={3} // Add margin bottom
+              />
+              <TextField
+                label="Password"
+                fullWidth
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                mb={3} // Add margin bottom
+              />
+              <TextField
+                label="Confirm Password"
+                fullWidths
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                mb={3} // Add margin bottom
+              />
+              <Button type="submit" variant="contained" color="primary" mt={6}>
+                {" "}
+                {/* Add margin top */}
+                Update
+              </Button>
+            </form>
+          </Grid>
+          <Grid item xs={12} md={9}>
+            <Typography variant="h4" mb={2}>
+              My Orders
+            </Typography>{" "}
+            {/* Add margin bottom */}
+            {loadingOrders ? (
+              <Loader />
+            ) : errorOrders ? (
+              <Message variant="danger">{errorOrders}</Message>
+            ) : (
+              <TableContainer>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>ID</TableCell>
+                      <TableCell>DATE</TableCell>
+                      <TableCell>TOTAL</TableCell>
+                      <TableCell>PAID</TableCell>
+                      <TableCell>DELIVERED</TableCell>
+                      <TableCell></TableCell>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          )}
+                  </TableHead>
+                  <TableBody>
+                    {orders.map((order) => (
+                      <TableRow key={order._id}>
+                        <TableCell>{order._id}</TableCell>
+                        <TableCell>
+                          {order.createdAt.substring(0, 10)}
+                        </TableCell>
+                        <TableCell>{order.totalPrice}</TableCell>
+                        <TableCell>
+                          {order.isPaid ? (
+                            order.paidAt.substring(0, 10)
+                          ) : (
+                            <i
+                              className="fas fa-times"
+                              style={{ color: "red" }}
+                            ></i>
+                          )}
+                        </TableCell>
+                        <TableCell>
+                          {order.isDelivered ? (
+                            order.deliveredAt.substring(0, 10)
+                          ) : (
+                            <i
+                              className="fas fa-times"
+                              style={{ color: "red" }}
+                            ></i>
+                          )}
+                        </TableCell>
+                        <TableCell>
+                          <Link to={`/order/${order._id}`}>
+                            <Button variant="outlined">Details</Button>
+                          </Link>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            )}
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </>
   );
 };
